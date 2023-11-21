b3dfc0480528012f860d53b5a9fcb1b4b562af3a
Ductai <luuductai12@gmail.com>
2023-11-21 20:58:02 +0700

    cau2

--- lap01.js
+++ lap01.js
@@ -1,1 +1,15 @@
 let arr =[3,2,5,6,4,8,9]
+let t = 0
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+            t = arr[i] 
+        } else {
+            t = arr[j]
+        }
+        
+    }
+        
+    }
+    
